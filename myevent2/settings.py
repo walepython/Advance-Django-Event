006910d93d6ffe4fce4231ac5ef3255aefae98b4
@waminisce <onidareadewaleoluwafemi69@gmail.com>
2026-02-23 07:20:15 +0100

    Fix cloudinary configuration

--- myevent2/settings.py
+++ myevent2/settings.py
@@ -127,12 +127,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+if not DEBUG:
     
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
